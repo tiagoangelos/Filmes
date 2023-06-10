f59104a5f439353b27aa0cb8ec0627ffcc67908e
Tiago Angelo <thiagoangelos2020@gmail.com>
2023-06-10 17:55:49 -0300

    removendo footer temporariamente

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import Navbar from './components/Navbar';
 import { Outlet } from 'react-router-dom';
-import Footer from './components/footer';
 
 function App() {
   return (
     <div className='App'>
       <Navbar />
       <Outlet />
-      <Footer />
     </div>
   )
 }
